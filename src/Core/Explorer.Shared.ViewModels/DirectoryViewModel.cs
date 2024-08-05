4a24df4c5176a8b5619228762e5aa14f1fd0fb79
Ynychar1980 <yanychar1980@gmail.com>
2024-08-06 00:22:39 +0300

    Реализована простое отображение файлов и папок в проекте на WPF.

---
+++ src/Core/Explorer.Shared.ViewModels/DirectoryViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Explorer.Shared.ViewModels
+{
+    public sealed class DirectoryViewModel : FileEntityViewModel
+    {
+        public DirectoryViewModel(string directoryName) : base(directoryName)
+        {
+            FullName = directoryName;
+        }
+
+        public DirectoryViewModel(DirectoryInfo directoryName) : base(directoryName.Name)
+        {
+            FullName = directoryName.FullName;
+        }
+    }
+}
--- src/Core/Explorer.Shared.ViewModels/MainViewModel.cs
+++ src/Core/Explorer.Shared.ViewModels/MainViewModel.cs
@@ -1,30 +1,74 @@
-﻿using System.ComponentModel;
-using System.Runtime.CompilerServices;
+﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
 
 namespace Explorer.Shared.ViewModels
 {
-    public class MainViewModel : INotifyPropertyChanged
+    public class MainViewModel : BaseViewModel
     {
         #region Public Properties
-        public string MainDiskName { get; set; }
+
+        public string? FilePath {  get; set; }
+
+        public ObservableCollection<FileEntityViewModel> DirectoriesAndFiles { get; set; } = 
+            new ObservableCollection<FileEntityViewModel>();
+
+        public FileEntityViewModel SelectedFileEntity { get; set; }
+
+        #endregion
+
+        #region Commands
+
+        public ICommand OpenCommand { get; }
+
         #endregion
 
         #region Events
-        public event PropertyChangedEventHandler? PropertyChanged;
+
         #endregion
 
         #region Constructor
+
         public MainViewModel()
         {
-            MainDiskName = Environment.SystemDirectory;
+            OpenCommand = new DelegateCommand(Open);
+
+            foreach (var logicalDrive in Directory.GetLogicalDrives())
+            {
+                DirectoriesAndFiles.Add(new DirectoryViewModel(logicalDrive));
+            }
         }
+
         #endregion
 
         #region Protected Methods
-        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+
+        #endregion
+
+        #region Commands Methods
+
+        private void Open(object parameter)
         {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            if (parameter is DirectoryViewModel directoryViewModel)
+            {
+                FilePath = directoryViewModel.FullName;
+
+                DirectoriesAndFiles.Clear();
+
+                var directoryInfo = new DirectoryInfo(FilePath);
+
+                foreach (var directory in directoryInfo.GetDirectories())
+                {
+                    DirectoriesAndFiles.Add(new DirectoryViewModel(directory));
+                }
+
+                foreach (var fileInfo in directoryInfo.GetFiles())
+                {
+                    DirectoriesAndFiles.Add(new FileViewModel(fileInfo));
+                }
+            }
+
         }
+
         #endregion
     }
 }
